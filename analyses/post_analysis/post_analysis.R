2cb2776c8d8b11571f13f280dbacfbb2a0b74b79
Wilhelm Sjöland <wilhelmsjoland@gmail.com>
2026-08-11 19:04:07 -0400

    Script for combining all hits

---
+++ analyses/post_analysis/post_analysis.R
@@ -0,0 +1,85 @@
+library(tidyverse)
+library(arrow)
+
+exp_dir <- "/Volumes/bluecub/aglycone_release_100um_24h/output"
+
+exp_names <- list.files(
+  exp_dir,
+  recursive = FALSE,
+  full.names = FALSE
+)
+
+all_hits <- exp_names %>%
+  purrr::map(
+    .x = .,
+    .f = ~ {
+      bio <- file.path(
+        exp_dir,
+        .x,
+        "report",
+        "biotransformer_similarities.parquet"
+      )
+
+      anno <- file.path(
+        exp_dir,
+        .x,
+        "report",
+        "annotation_similarities.parquet"
+      )
+
+      anno_wrangle <- arrow::read_parquet(file = anno) %>%
+        dplyr::mutate(
+          experiment = .x,
+          source = "anno",
+          sim_mol = NA
+        ) %>%
+        dplyr::select(
+          "experiment",
+          "source",
+          "sim_mol",
+          "feature" = peak_id,
+          "feature_pred" = feature,
+          "adduct",
+          "mass" = "exact_mass",
+          "mz",
+          "rtime",
+          "sim",
+          "formula" = "target_formula",
+          "smiles" = "target_smiles",
+          "inchikey" = "target_inchikey",
+          "ppm_error"
+        )
+
+      bio_wrangle <- arrow::read_parquet(file = bio) %>%
+        dplyr::mutate(
+          experiment = .x,
+          source = "bio",
+          feature_pred = paste0(feature, ".", met_id),
+          ppm_error = abs((mzmed - mz) / mz * 1e6)
+        ) %>%
+        dplyr::select(
+          "experiment",
+          "source",
+          "sim_mol",
+          "feature",
+          "feature_pred" = feature_pred,
+          "adduct",
+          "mass",
+          "mz" = "mzmed",
+          "rtime" = "rtmed",
+          "sim",
+          "formula" = `Molecular formula`,
+          "smiles" = "SMILES",
+          "inchikey",
+          "ppm_error"
+        )
+
+      dplyr::bind_rows(
+        ... = list(
+          anno_wrangle,
+          bio_wrangle
+        )
+      )
+    }
+  ) %>%
+  dplyr::bind_rows()
